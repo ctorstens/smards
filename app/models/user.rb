2c53358e74c0821b35a8dbfb2b531f797c096ebf
Chris Torstenson <itunes.torstenson@gmail.com>
2013-04-27 09:49:02 -0700

    add authentication to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,31 @@ class User < ActiveRecord::Base
   validates :email, :presence => true
   validates :user_name, :presence => true
   validates :password_hash, :presence => true
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(g_username, g_password)
+    user = User.find_by_user_name(g_username)
+    return nil unless user
+    if user.password == g_password
+      user.create_token
+    else
+      return nil
+    end
+  end
+
+  def create_token
+    token = Digest::MD5.hexdigest(self.email + Time.now.to_s + "kjsd94*asdHH*&h80fh")
+    return token
+  end
+
 end
